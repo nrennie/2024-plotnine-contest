5c25fee6c5ad387ac6a294ae28c85f3745e5a4c6
nrennie <nrennie35@gmail.com>
2024-06-26 17:15:23 +0100

    create initial carbon emission plot

--- plot.py
+++ plot.py
@@ -3,158 +3,178 @@
 
 import plotnine as gg
 import pandas as pd
-import numpy as np
-from string import Template
+import textwrap
+import matplotlib.pyplot as plt
+import highlight_text as ht
+
 
 # Load data ---------------------------------------------------------------
 
-events = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-27/events.csv')
-births = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-27/births.csv')
-deaths = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-02-27/deaths.csv')
+emissions = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-21/emissions.csv')
 
 
-# Data wrangling
+# Data wrangling ----------------------------------------------------------
 
-# Mutate and transform the dataframe
-plot_births = births.copy()
-plot_births['type'] = 'birth'
-plot_births = plot_births.rename(columns={'year_birth': 'year'})
-plot_births = plot_births[plot_births['year'] >= 1900]
+# Prep data for plotting
+plot_data = emissions[
+    emissions['commodity'].isin([
+        "Sub-Bituminous Coal", "Metallurgical Coal", "Bituminous Coal",
+        "Thermal Coal", "Anthracite Coal", "Lignite Coal"
+    ])
+].copy()
+plot_data['commodity'] = plot_data['commodity'].str.replace(' Coal', '')
+plot_data = plot_data[['year', 'commodity', 'production_value']]
+plot_data = plot_data.groupby(['year', 'commodity'], as_index=False).agg({'production_value': 'sum'}).rename(columns={'production_value': 'n'})
+plot_data = plot_data[plot_data['year'] >= 1900]
+exceeds100 = plot_data.groupby('year')['n'].sum()
+exceeds100 = exceeds100[exceeds100 > 100].index.min()
+orders = plot_data[plot_data['year'] == 2022].sort_values(by='n', ascending=False)['commodity']
+plot_data['commodity'] = pd.Categorical(plot_data['commodity'], categories=orders, ordered=True)
+plot_data = plot_data.sort_values(by=['year', 'commodity'])
 
-# Using a template string for the label
-def create_label(row):
-    return f"{row['person']} ({row['description'].strip()})"
-
-plot_births['label'] = plot_births.apply(create_label, axis=1)
-
-# Calculations for theta, x, y, and angle
-theta = np.linspace(np.pi / 4, (7 / 4) * np.pi, num=len(plot_births))
-plot_births['theta'] = theta
-plot_births['x'] = 6 * np.cos(theta)
-plot_births['y'] = 6 * np.sin(theta)
-plot_births['angle'] = 180 + 360 * (theta / (2 * np.pi))
-
-plot_births <- births |>
-  mutate(type = "birth") |>
-  rename(year = year_birth) |>
-  filter(year >= 1900) |>
-  mutate(label = glue("{person} ({str_trim(description)})")) %>%
-  mutate(
-    theta = seq(pi / 4, (7 / 4) * pi, length.out = nrow(.)),
-    x = 6 * cos(theta),
-    y = 6 * sin(theta),
-    angle = 180 + 360 * (theta / (2 * pi))
-  )
-
-plot_deaths <- deaths |>
-  mutate(type = "death") |>
-  rename(year = year_death) |>
-  filter(year >= 1900) |>
-  mutate(label = glue("{person} ({str_trim(description)})")) %>%
-  mutate(
-    theta = seq(pi / 4, (7 / 4) * pi, length.out = nrow(.)),
-    x = 3 * cos(theta),
-    y = 3 * sin(theta),
-    angle = 180 + 360 * (theta / (2 * pi))
-  )
-
-# Load fonts --------------------------------------------------------------
-
-# font_add_google('Roboto', 'roboto')
-# font_add_google('Roboto Slab', 'roboto_slab')
-# showtext_auto()
+# Create data for x-axis labels
+segment_data = pd.DataFrame({
+    'year': list(range(1900, 2021, 20))
+})
+text_data = pd.DataFrame({
+    'year': list(range(1900, 2021, 20)),
+    'label': list(range(1900, 2021, 20))
+})
 
 
 # Define colours and fonts-------------------------------------------------
 
-bg_col = '#fafafa'
-text_col = 'gray5'
-highlight_col = '#35978f'
+bg_col = "#FFFFFA"
+text_col = "#0D5C63"
+col_palette = ["#E58606", "#5D69B1", "#52BCA3", "#99C945", "#CC61B0", "#24796C"]
+highlight_col = col_palette[2]
 
-body_font = 'roboto'
-title_font = 'roboto_slab'
-
-
+body_font = "Arial"
 
 # Define text -------------------------------------------------------------
 
-social = nrBrand::social_caption(
-  bg_colour = bg_col,
-  icon_colour = highlight_col,
-  font_colour = text_col,
-  font_family = body_font
-)
-title = 'Take a leap! Births and deaths on February 29<sup>th</sup>'
-st = glue('February 29 is a leap day (or 'leap year day'), an intercalary date
-added periodically to create leap years in the Julian and Gregorian calendars. 
-Wikpedia lists {nrow(dplyr::filter(births, year_birth >= 1900))}
-<span style='color: {highlight_col};'>births</span> and {nrow(dplyr::filter(deaths, year_death >= 1900))}
-<span style='color: #bf812d;'>deaths</span> on a leap day since 1900.')
-cap = paste0(
-  '**Data**: Wikipedia<br>**Graphic**:', social
-)
+# title, subtitle
+title_text = "Coal production since 1900"
+st = "Carbon Majors is a database of historical production data from 122 of the world’s largest oil, gas, coal, and cement producers. This data is used to quantify the direct operational emissions and emissions from the combustion of marketed products that can be attributed to these entities."
+wrapped_subtitle = "\n".join(textwrap.wrap(st, width=60))
+
+# annotation labels
+coal_types_label = 'Total coal production\nincludes production of <Bituminous::{"color": "#E58606"}>, <Sub-bituminous::{"color": "#5D69B1"}>, <Metallurgical::{"color": "#52BCA3"}>, <Lignite::{"color": "#99C945"}>, <Anthracite::{"color": "#CC61B0"}>, and <Thermal::{"color": "#24796C"}> coal. Bituminous accounts for around half.'
+
+# caption
+cap = '<Data::{"fontweight": "bold"}>: Carbon Majors\n<Graphic::{"fontweight": "bold"}>: nrennie'
 
 
 # Plot --------------------------------------------------------------------
 
-ggplot() +
-  geom_textcircle(
-    data = dplyr::filter(births, year_birth >= 1900),
-    mapping = aes(label = person),
-    colour = highlight_col,
-    r = 6,
-    family = body_font,
-    size = 6,
-  ) +
-  geom_textcircle(
-    data = dplyr::filter(deaths, year_death >= 1900),
-    mapping = aes(label = person),
-    colour = '#bf812d',
-    r = 3,
-    family = body_font,
-    size = 6,
-  ) +
-  # add title and subtitle
-  geom_textbox(
-    data = data.frame(x = 0, y = 1.2, label = title),
-    mapping = aes(x = x, y = y, label = label),
-    hjust = 0,
-    colour = text_col,
-    family = title_font,
-    fontface = 'bold',
-    lineheight = 0.5,
-    fill = 'transparent',
-    box.colour = 'transparent',
-    size = 11,
-    minwidth = 0.5
-  ) +
-  geom_textbox(
-    data = data.frame(x = 0, y = -0.5, label = st),
-    mapping = aes(x = x, y = y, label = label),
-    hjust = 0,
-    colour = text_col,
-    family = body_font,
-    lineheight = 0.5,
-    fill = 'transparent',
-    box.colour = 'transparent',
-    size = 9,
-    minwidth = 0.45
-  ) +
-  scale_x_continuous(limits = c(-8, 8)) +
-  scale_y_continuous(limits = c(-8, 8)) +
-  labs(caption = cap) +
-  theme_void(base_size = 30, base_family = body_font) +
-  theme(
-    plot.margin = margin(5, 5, 5, 5),
-    plot.background = element_rect(fill = bg_col, colour = bg_col),
-    panel.background = element_rect(fill = bg_col, colour = bg_col),
-    plot.caption = element_textbox_simple(
-      colour = text_col,
-      hjust = 0,
-      halign = 0,
-      margin = margin(b = 5, t = -5, l = 5),
-      lineheight = 0.5,
-      family = body_font
-    )
-  )
+p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
+     # Axis labels
+     gg.geom_segment(data=segment_data, mapping=gg.aes(x='year', xend='year', y=0, yend=-1700),
+                     linetype='dashed', alpha=0.4, color=text_col) +
+     gg.geom_text(data=text_data, mapping=gg.aes(x='year', y=-1900, label='label'),
+                  color=text_col, size=8, family=body_font) +
+     # Annotation 1
+     gg.annotate(
+         "segment",
+         x=exceeds100, xend=exceeds100,
+         y=0, yend=5000,
+         size=1,
+         color=text_col
+     ) +
+     gg.annotate(
+         "text",
+         x=exceeds100+2, y=5000,
+         label=exceeds100,
+         color=text_col,
+         family=body_font,
+         ha="left",
+         va="top",
+         size=12,
+         fontweight="bold"
+     ) +
+     gg.annotate(
+         "text",
+         x=exceeds100+2, y=5000-600,
+         label="Total coal production first exceeds 100\nmillion tonnes per year.",
+         color=text_col,
+         family=body_font,
+         ha="left",
+         va="top"
+     ) +
+     # Annotation 2
+     gg.annotate(
+         "segment",
+         size=1,
+         x=1990, xend=1990,
+         y=0, yend=10000,
+         color=text_col
+     ) +
+     gg.annotate(
+         "text",
+         x=1990+2, y=10000,
+         label="Coal types",
+         color=text_col,
+         family=body_font,
+         ha="left",
+         va="top",
+         size=12,
+         fontweight="bold"
+     ) +
+     # Area plot
+     gg.geom_area(gg.aes(fill='commodity')) +
+     # Scales
+     gg.scale_fill_manual(values=col_palette) +
+     gg.scale_x_continuous(limits=(1896, 2025)) +
+     gg.scale_y_continuous(limits=(-2400, 12000)) +
+     # Text
+     gg.annotate(
+         "text",
+         x=1900, y=11400,
+         label=title_text,
+         color=text_col,
+         family=body_font,
+         ha="left",
+         va="top",
+         size=20,
+         fontweight="bold"
+     ) +
+     gg.annotate(
+         "text",
+         x=1900, y=10500,
+         label=wrapped_subtitle,
+         color=text_col,
+         family=body_font,
+         size=14,
+         ha="left",
+         va="top"
+     ) +
+     # Styling
+     gg.coord_cartesian(expand=False) +
+     gg.theme_void(base_size=8) +
+     gg.theme(
+         legend_position='none',
+         plot_background=gg.element_rect(fill=bg_col, color=bg_col),
+         panel_background=gg.element_rect(fill=bg_col, color=bg_col)
+     ))
+
+# save image
+gg.ggsave(p, width=8, height=6, filename='plot.png', dpi=300)
+
+# Add matplotlib text
+fig = p.draw()
+fig.set_size_inches(16, 12, forward=True)
+fig.set_dpi(300)
+ax = plt.gca()
+ht.ax_text(1952, 9000, coal_types_label, color=text_col, fontsize=18, va='top')
+ht.ax_text(1900, -2300, cap, color=text_col, fontsize=18, va='top')
+plt.show()
+plt.savefig('plot.png', dpi = 300)
+
+
+# TODO: 
+# saving size / arranging
+# y-axis labels on right hand side?
+# documentation blog post
+# create reprex and fix commenting
+# submit
 
