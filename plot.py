25dbdea55e6ef0508f10bd6f11e33ff3dc28d5f5
nrennie <nrennie35@gmail.com>
2024-06-26 17:48:26 +0100

    tidy up

--- plot.py
+++ plot.py
@@ -10,69 +10,70 @@ import highlight_text as ht
 
 # Load data ---------------------------------------------------------------
 
-emissions = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-21/emissions.csv')
+emissions=pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-05-21/emissions.csv')
 
 
 # Data wrangling ----------------------------------------------------------
 
 # Prep data for plotting
-plot_data = emissions[
+plot_data=emissions[
     emissions['commodity'].isin([
         "Sub-Bituminous Coal", "Metallurgical Coal", "Bituminous Coal",
         "Thermal Coal", "Anthracite Coal", "Lignite Coal"
     ])
 ].copy()
-plot_data['commodity'] = plot_data['commodity'].str.replace(' Coal', '')
-plot_data = plot_data[['year', 'commodity', 'production_value']]
-plot_data = plot_data.groupby(['year', 'commodity'], as_index=False).agg({'production_value': 'sum'}).rename(columns={'production_value': 'n'})
-plot_data = plot_data[plot_data['year'] >= 1900]
-exceeds100 = plot_data.groupby('year')['n'].sum()
-exceeds100 = exceeds100[exceeds100 > 100].index.min()
-orders = plot_data[plot_data['year'] == 2022].sort_values(by='n', ascending=False)['commodity']
-plot_data['commodity'] = pd.Categorical(plot_data['commodity'], categories=orders, ordered=True)
-plot_data = plot_data.sort_values(by=['year', 'commodity'])
+plot_data['commodity']=plot_data['commodity'].str.replace(' Coal', '')
+plot_data=plot_data[['year', 'commodity', 'production_value']]
+plot_data=plot_data.groupby(['year', 'commodity'], as_index=False).agg({'production_value': 'sum'}).rename(columns={'production_value': 'n'})
+plot_data=plot_data[plot_data['year'] >= 1900]
+exceeds100=plot_data.groupby('year')['n'].sum()
+exceeds100=exceeds100[exceeds100 > 100].index.min()
+orders=plot_data[plot_data['year'] == 2022].sort_values(by='n', ascending=False)['commodity']
+plot_data['commodity']=pd.Categorical(plot_data['commodity'], categories=orders, ordered=True)
+plot_data=plot_data.sort_values(by=['year', 'commodity'])
 
 # Create data for x-axis labels
-segment_data = pd.DataFrame({
+segment_data=pd.DataFrame({
     'year': list(range(1900, 2021, 20))
 })
-text_data = pd.DataFrame({
-    'year': list(range(1900, 2021, 20)),
-    'label': list(range(1900, 2021, 20))
+y_axis_data=pd.DataFrame({
+    'value': [0, 2000, 4000, 6000, 8000],
+    'label': ['0', '2,000', '4,000', '6,000', '8,000\nmillion\ntonnes']
 })
-
 
 # Define colours and fonts-------------------------------------------------
 
-bg_col = "#FFFFFA"
-text_col = "#0D5C63"
-col_palette = ["#E58606", "#5D69B1", "#52BCA3", "#99C945", "#CC61B0", "#24796C"]
-highlight_col = col_palette[2]
+bg_col="#FFFFFA"
+text_col="#0D5C63"
+col_palette=["#E58606", "#5D69B1", "#52BCA3", "#99C945", "#CC61B0", "#24796C"]
 
-body_font = "Arial"
+body_font="Arial"
 
 # Define text -------------------------------------------------------------
 
 # title, subtitle
-title_text = "Coal production since 1900"
-st = "Carbon Majors is a database of historical production data from 122 of the world’s largest oil, gas, coal, and cement producers. This data is used to quantify the direct operational emissions and emissions from the combustion of marketed products that can be attributed to these entities."
-wrapped_subtitle = "\n".join(textwrap.wrap(st, width=60))
+title_text="Coal production since 1900"
+st="Carbon Majors is a database of historical production data from 122 of the world’s largest oil, gas, coal, and cement producers. This data is used to quantify the direct operational emissions and emissions from the combustion of marketed products that can be attributed to these entities."
+wrapped_subtitle="\n".join(textwrap.wrap(st, width=55))
 
 # annotation labels
-coal_types_label = 'Total coal production\nincludes production of <Bituminous::{"color": "#E58606"}>, <Sub-bituminous::{"color": "#5D69B1"}>, <Metallurgical::{"color": "#52BCA3"}>, <Lignite::{"color": "#99C945"}>, <Anthracite::{"color": "#CC61B0"}>, and <Thermal::{"color": "#24796C"}> coal. Bituminous accounts for around half.'
+coal_types_label='Total coal production includes\nproduction of <Bituminous::{"color": "#E58606"}>,\n<Sub-bituminous::{"color": "#5D69B1"}>, <Metallurgical::{"color": "#52BCA3"}>, <Lignite::{"color": "#99C945"}>,\n<Anthracite::{"color": "#CC61B0"}>, and <Thermal::{"color": "#24796C"}> coal.\nBituminous accounts for\naround half.'
 
 # caption
-cap = '<Data::{"fontweight": "bold"}>: Carbon Majors\n<Graphic::{"fontweight": "bold"}>: nrennie'
+cap='<Data::{"fontweight": "bold"}>: Carbon Majors\n<Graphic::{"fontweight": "bold"}>: Nicola Rennie (GitHub: @nrennie)'
 
 
 # Plot --------------------------------------------------------------------
 
-p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
-     # Axis labels
+p=(gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
+     # Axis lines
      gg.geom_segment(data=segment_data, mapping=gg.aes(x='year', xend='year', y=0, yend=-1700),
                      linetype='dashed', alpha=0.4, color=text_col) +
-     gg.geom_text(data=text_data, mapping=gg.aes(x='year', y=-1900, label='label'),
-                  color=text_col, size=8, family=body_font) +
+     # Axis labels
+     gg.geom_text(data=segment_data, mapping=gg.aes(x='year', y=-1900, label='year'),
+                  color=text_col, size=8, family=body_font, ha="left") +
+     gg.geom_text(data=y_axis_data, mapping=gg.aes(x=2023, y='value', label='label'),
+                  color=text_col, size=8, family=body_font, ha="left") +
      # Annotation 1
      gg.annotate(
          "segment",
@@ -89,43 +90,44 @@ p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
          family=body_font,
          ha="left",
          va="top",
-         size=12,
+         size=10,
          fontweight="bold"
      ) +
      gg.annotate(
          "text",
          x=exceeds100+2, y=5000-600,
-         label="Total coal production first exceeds 100\nmillion tonnes per year.",
+         label="Total coal production first\nexceeds 100 million tonnes\nper year.",
          color=text_col,
          family=body_font,
          ha="left",
+         size=9,
          va="top"
      ) +
      # Annotation 2
      gg.annotate(
          "segment",
          size=1,
-         x=1990, xend=1990,
+         x=1975, xend=1975,
          y=0, yend=10000,
          color=text_col
      ) +
      gg.annotate(
          "text",
-         x=1990+2, y=10000,
+         x=1975+2, y=10000,
          label="Coal types",
          color=text_col,
          family=body_font,
          ha="left",
          va="top",
-         size=12,
+         size=10,
          fontweight="bold"
      ) +
      # Area plot
      gg.geom_area(gg.aes(fill='commodity')) +
      # Scales
      gg.scale_fill_manual(values=col_palette) +
-     gg.scale_x_continuous(limits=(1896, 2025)) +
-     gg.scale_y_continuous(limits=(-2400, 12000)) +
+     gg.scale_x_continuous(limits=(1896, 2034)) +
+     gg.scale_y_continuous(limits=(-3300, 12000)) +
      # Text
      gg.annotate(
          "text",
@@ -135,7 +137,7 @@ p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
          family=body_font,
          ha="left",
          va="top",
-         size=20,
+         size=12,
          fontweight="bold"
      ) +
      gg.annotate(
@@ -144,7 +146,7 @@ p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
          label=wrapped_subtitle,
          color=text_col,
          family=body_font,
-         size=14,
+         size=9.5,
          ha="left",
          va="top"
      ) +
@@ -157,24 +159,12 @@ p = (gg.ggplot(plot_data, gg.aes(x='year', y='n')) +
          panel_background=gg.element_rect(fill=bg_col, color=bg_col)
      ))
 
-# save image
-gg.ggsave(p, width=8, height=6, filename='plot.png', dpi=300)
-
-# Add matplotlib text
-fig = p.draw()
+# Add styled text with matplotlib and HighlightText
+fig=p.draw()
 fig.set_size_inches(16, 12, forward=True)
 fig.set_dpi(300)
-ax = plt.gca()
-ht.ax_text(1952, 9000, coal_types_label, color=text_col, fontsize=18, va='top')
-ht.ax_text(1900, -2300, cap, color=text_col, fontsize=18, va='top')
+ax=plt.gca()
+ht.ax_text(1977, 9400, coal_types_label, vsep=3, color=text_col, fontname=body_font, fontsize=9, va='top')
+ht.ax_text(1900, -2300, cap, color=text_col, fontname=body_font, fontsize=7.5, va='top')
 plt.show()
-plt.savefig('plot.png', dpi = 300)
-
-
-# TODO: 
-# saving size / arranging
-# y-axis labels on right hand side?
-# documentation blog post
-# create reprex and fix commenting
-# submit
-
+plt.savefig('plot.png', dpi=300)
